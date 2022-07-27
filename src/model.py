316bf8d83a8e8d68671d4b766d5e53d7a725473b
tangzl7 <3155837867@qq.com>
2022-07-27 10:06:20 +0800

    .

--- src/model.py
+++ src/model.py
@@ -1,3 +1,4 @@
+from turtle import forward
 import torch
 import torch.nn as nn
 import torchvision.transforms.functional as TF
@@ -66,57 +67,95 @@ class Sample(nn.Module):
         return out
 
 class Encoder(nn.Module):
-    def __init__(self, conv_cnt, n_feat, kernel_size, reduction, act, bias, scale_unet_feats, scale_factor):
+    def __init__(self, conv_cnt, n_feat, kernel_size, act, bias, scale_unet_feats, scale_factor):
         super(Encoder, self).__init__()
-        modules_body = []
-        for i in range(conv_cnt):
-            modules_body.append(nn.Conv2d(n_feat, scale_unet_feats * (2 ** i), kernel_size, padding='same' , bias=bias))
-            modules_body.append(act)
-            modules_body.append(Sample(scale_factor))
-            n_feat = scale_unet_feats * (2 ** i)
-        self.encode = nn.Sequential(*modules_body)
+        self.enc1 = nn.Conv2d(n_feat, scale_unet_feats, kernel_size, padding='same' , bias=bias)
+        self.enc2 = nn.Conv2d(scale_unet_feats, scale_unet_feats * 2, kernel_size, padding='same' , bias=bias)
+        self.enc3 = nn.Conv2d(scale_unet_feats * 2, scale_unet_feats * 4, kernel_size, padding='same' , bias=bias)
+        self.act = act
+        self.down = Sample(scale_factor)
 
     def forward(self, x):
-        out = self.encode(x)
-        return out
+        enc1 = self.enc1(x)
+        x = self.act(enc1)
+        x = self.down(x)
+        enc2 = self.enc2(x)
+        x = self.act(enc2)
+        x = self.down(x)
+        enc3 = self.enc3(x)
+        return enc1, enc2, enc3
 
 class Decoder(nn.Module):
-    def __init__(self, conv_cnt, n_feat, kernel_size, reduction, act, bias, scale_unet_feats, scale_factor):
+    def __init__(self, conv_cnt, n_feat, kernel_size, act, bias, scale_unet_feats, scale_factor):
         super(Decoder, self).__init__()
         modules_body = []
-        for i in range(conv_cnt-1, -1, -1):
-            if i == 0:
-                modules_body.append(nn.Conv2d(scale_unet_feats * (2 ** i), n_feat, kernel_size, padding='same' , bias=bias))
-            else:
-                modules_body.append(nn.Conv2d(scale_unet_feats * (2 ** i), scale_unet_feats * (2 ** (i-1)), kernel_size, padding='same' , bias=bias))
-            modules_body.append(act)
-            modules_body.append(Sample(scale_factor))
-        self.decode = nn.Sequential(*modules_body)
+        self.dec1 = nn.Conv2d(scale_unet_feats * 4, scale_unet_feats * 2, kernel_size, padding='same' , bias=bias)
+        self.dec2 = nn.Conv2d(scale_unet_feats * 2, scale_unet_feats, kernel_size, padding='same' , bias=bias)
+        self.dec3 = nn.Conv2d(scale_unet_feats, n_feat, kernel_size, padding='same' , bias=bias)
+        self.act = act
+        self.up = Sample(scale_factor)
     
     def forward(self, x):
-        out = self.decode(x)
+        enc1, enc2, enc3 = x
+        dec1 = self.dec1(enc3)
+        t = enc2 + self.up(dec1)
+        dec2 = self.dec2(t)
+        t = enc1 + self.up(dec2)
+        out = self.dec3(t)
         return out
 
 
+class FNet(nn.Module):
+    def __init__(self, conv_cnt, scale_feats, n_feat, kernel_size, reduction, bias, act):
+        super(FNet, self).__init__()
+        modules = []
+        modules.append(CABlock(scale_feats * (2 ** (conv_cnt-1)), kernel_size, reduction, bias, act))
+        for i in range(conv_cnt-1, -1, -1):
+            if i == 0:
+                modules.append(nn.Conv2d(scale_feats * (2 ** i), n_feat, kernel_size, padding='same' , bias=bias))
+            else:
+                modules.append(nn.Conv2d(scale_feats * (2 ** i), scale_feats * (2 ** (i-1)), kernel_size, padding='same', bias=bias))
+            modules.append(act)
+        
+        self.fusion = nn.Sequential(*modules)
+    
+    def forward(self, x, y):
+        out = x + y
+        out = self.fusion(out)
+        return out
+
 class FusionNet(nn.Module):
-    def __init__(self, vis_c=3, nir_c=1, out_c=3, feat_conv_cnt=3, feat_scale_factor=16, kernel_size=3, reduction=4, bias=False):
+    def __init__(self, vis_c=3, nir_c=1, out_c=3, feat_conv_cnt=3, feat_scale_factor=16, coder_conv_cnt=3, scale_unet_feats=16, kernel_size=3, reduction=4, bias=False):
         super(FusionNet, self).__init__()
         act = nn.PReLU()
         self.vis_feat_extractor = FeatExtractor(feat_conv_cnt, feat_scale_factor, vis_c, kernel_size, reduction, bias, act)
         self.nir_feat_extractor = FeatExtractor(feat_conv_cnt, feat_scale_factor, nir_c, kernel_size, reduction, bias, act)
+        self.encoder = Encoder(coder_conv_cnt, vis_c, kernel_size, act, bias, scale_unet_feats, scale_factor=0.5)
+        self.decoder = Decoder(coder_conv_cnt, out_c, kernel_size, act, bias, scale_unet_feats, scale_factor=2)
+        self.decoder_tail = nn.Conv2d(out_c, out_c, kernel_size, padding='same')
+        self.fnet = FNet(feat_conv_cnt, feat_scale_factor, out_c, kernel_size, reduction, bias, act)
+        self.fnet_tail = nn.Conv2d(out_c, out_c, kernel_size, padding='same')
 
     def forward(self, vis, nir):                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
         feat_1 = self.vis_feat_extractor(vis)
         feat_2 = self.nir_feat_extractor(nir)
-        return feat_1, feat_2
+        fusion = self.fnet(feat_1, feat_2)
+
+        coder_out = self.encoder(vis)
+        coder_out = self.decoder(coder_out)
+        coder_out = self.decoder_tail(coder_out)
+
+        out = fusion + coder_out
+        out = self.fnet_tail(out)
+        return out
 
 
 if __name__ == '__main__':
     fusion_net = FusionNet()
     rgb, nir = Image.open('../data/0001_rgb.jpg'), Image.open('../data/0001_nir.jpg')
     rgb, nir = TF.to_tensor(rgb), TF.to_tensor(nir)
-    nir = nir[0, :, :].reshape(1, 2160, 3840)
+    rgb = rgb.reshape([1, rgb.shape[0], rgb.shape[1], rgb.shape[2]])
+    nir = nir[0, :, :].reshape([1, 1, nir.shape[1], nir.shape[2]])
     print(fusion_net)
-    out1, out2 = fusion_net(rgb, nir)
-    print(out2.shape)
-    pass
+    out = fusion_net(rgb, nir)
+    print(out.shape)
